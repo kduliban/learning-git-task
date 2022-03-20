5e3eafcbb8047b642ab53fab26b8ce4824eef41b
kasiaduliban@gmail.com <kasiaduliban@gmail.com>
2022-03-20 20:00:12 +0100

    Count products

--- shop_list.py
+++ shop_list.py
@@ -2,9 +2,12 @@ lista_zakupow = {
     "piekarnia": ["chleb", "pączek", "bułki"],
     "warzywaniak": ["marchew", "seler", "rukola"]
 }
+x = 0
 for shop, products in lista_zakupow.items():
     shop = shop.title ()
     products_titled = []
     for product in products:
         products_titled.append (product.title())
+        x = x + 1
     print(f"Idę do {shop}, kupuję tu następujące rzeczy: {products_titled}.")
+print (f"W sumie kupuję {x} produktów")
