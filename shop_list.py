979bf66e8de340e1e736053390b4475c5b2d75f0
kasiaduliban@gmail.com <kasiaduliban@gmail.com>
2022-03-20 19:58:28 +0100

    Add loop

--- shop_list.py
+++ shop_list.py
@@ -2,3 +2,9 @@ lista_zakupow = {
     "piekarnia": ["chleb", "pączek", "bułki"],
     "warzywaniak": ["marchew", "seler", "rukola"]
 }
+for shop, products in lista_zakupow.items():
+    shop = shop.title ()
+    products_titled = []
+    for product in products:
+        products_titled.append (product.title())
+    print(f"Idę do {shop}, kupuję tu następujące rzeczy: {products_titled}.")
